b9f092f5525700dd58e072612bd186fdbd28cf6e
AmohamedNur <302881042@student.rocmondriaan.nl>
2022-09-05 12:56:22 +0200

    Initial commit

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE>
+<html lang="nl">
+  <head>
+      <title>Contact</title>
+      <link rel="stylesheet" href="style.css">
+      <!-- CSS only -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+            rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container-fluid bg-dark">
+      <div class="row">
+  <ul class="nav">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" href="#">Bestellen</a>
+      </li>
+  </ul>
+   </div>
+  </div>
+      <div class="container">
+          <div class="row">
+              <img src="img/shushi.png">
+          </div>
+      </div>
+    <div class="container  text-center">
+        <div class="row">
+           <div class="col">
+               <h1>Goedemiddag, welkom bij ZuZu</h1>
+               <p class="text">Wij zijn gespecialiseerd in de keuken.
+                   <br>
+               Het woord "sushi" is afkomstig van "su", wat azijn betekent,en "shi" - rijst.
+               </p>
+           </div>
+        </div>
+    </div>
+  <div class="container-md">
+      <div class="row">
+          <div class="col-6">
+              <img src="img/shushi2.png">
+          </div>
+          <div class="col-6">
+              <img src="img/shishi3.png">
+          </div>
+      </div>
+  </div>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+?>
